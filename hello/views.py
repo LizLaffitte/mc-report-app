3da4a7a08b16e3bb80c0e8c3e9bb678754ba7611
LizLaffitte <52801558+LizLaffitte@users.noreply.github.com>
2019-09-25 15:36:54 -0400

    Added print to views.py

--- hello/views.py
+++ hello/views.py
@@ -9,6 +9,7 @@ def index(request):
     r = requests.get('http://httpbin.org/status/418')
     print(r.text)
     return HttpResponse('<pre>' + r.text + '</pre>')
+    print("Test")
 
 
 def db(request):
